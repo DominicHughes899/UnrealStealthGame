3a846a80e41f4c6ae90827398ced6d9f28f41203
DominicHughes899 <91923512+DominicHughes899@users.noreply.github.com>
2024-01-19 23:42:01 +0000

    Created Interactee
    A general class that can be activated by Interactables.

---
+++ StealthGame/Source/StealthGame/Interactee.cpp
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interactee.h"
+
+// Sets default values
+AInteractee::AInteractee()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AInteractee::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AInteractee::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void AInteractee::LeverStateChange_Implementation(bool NewState)
+{
+	Activated = NewState;
+
+	OnStateChange();
+}
+
+void AInteractee::ButtonStateChange_Implementation(bool NewState)
+{
+	Activated = NewState;
+
+	OnStateChange();
+}
+
+
+
---
+++ StealthGame/Source/StealthGame/Interactee.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+
+#include "InteractInterface.h"
+
+#include "Interactee.generated.h"
+
+UCLASS()
+class STEALTHGAME_API AInteractee : public AActor, public IInteractInterface
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AInteractee();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	bool Activated = false;
+
+
+	// ==== Interface functions ====
+	void LeverStateChange_Implementation(bool NewState) override;
+
+	void ButtonStateChange_Implementation(bool NewState) override;
+
+
+	/**/
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnStateChange();
+
+};
